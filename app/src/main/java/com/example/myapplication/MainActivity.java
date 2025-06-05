9fac9e668fc7f3d16d841571738f07cd8a861016
verpii <verpeleti.nandor@gmail.com>
2025-06-05 14:56:53 +0700

    add commit

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,4 +1,42 @@
 package com.example.myapplication;
 
-public class MainActivity {
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private WorkoutCardAdapter workoutAdapter;
+    private List<WorkoutCard> workoutList;
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.homepage);
+
+            recyclerView = findViewById(R.id.recyclerViewWorkouts);
+            recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL,false));
+
+            Bitmap pushupBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.pushup_card);
+            Bitmap RunningBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.running_card);
+            Bitmap PlankBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.plank_card);
+
+        // Sample data
+        workoutList = new ArrayList<>();
+        workoutList.add(new WorkoutCard("Push-ups", "3 sets of 15 reps", 50,pushupBitmap ));
+        workoutList.add(new WorkoutCard("Running", "5 km run", 300,RunningBitmap));
+        workoutList.add(new WorkoutCard("Plank", "Hold for 1 minute", 30,PlankBitmap));
+
+        // Set adapter
+        workoutAdapter = new WorkoutCardAdapter(workoutList);
+        recyclerView.setAdapter(workoutAdapter);
+    }
+
+
 }
--- app/src/main/java/com/example/myapplication/WorkoutCard.java
+++ app/src/main/java/com/example/myapplication/WorkoutCard.java
@@ -1,4 +1,26 @@
 package com.example.myapplication;
 
+import android.graphics.Bitmap;
+
 public class WorkoutCard {
+    private String title;
+    private String details;
+    private int calories;
+
+    private Bitmap image;
+
+    public WorkoutCard(String title, String details, int calories, Bitmap image) {
+        this.title = title;
+        this.details = details;
+        this.calories = calories;
+        this.image = image;
+    }
+
+    public String getTitle() { return title; }
+    public String getDetails() { return details; }
+    public int getCalories() { return calories; }
+
+    public Bitmap getImage() { return image;}
+
+
 }
